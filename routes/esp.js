d631bb910066b8deeceadc63e30491b51ea03596
Vitor Avelino <vitoravelinosena1@gmail.com>
2020-05-19 20:02:49 -0300

    Funcionando cadstro e descadastro

--- routes/esp.js
+++ routes/esp.js
@@ -8,7 +8,8 @@ routeresp.get("/test", (req, res) => {
     //usudb.connection.query("select * from usuario", function (err, posts, field) {
        // if (err) throw err;
        // res.json({ nome: 'tobi', valor:'123'})//, valor:5,time:2})
-       res.send("a\n908a75a3\n1")
+       res.send("a\n908a75a3\n2")
+
 
         //render('admin/usuario', { posts: posts })
    // })    
@@ -16,3 +17,13 @@ routeresp.get("/test", (req, res) => {
 
 
 module.exports = routeresp
+/*
+*res.send("a\n908a75a3\n2")
+* Comando \n Parâmetro 1 \n Parâmetro 2
+* Comando:
+* 1 -) Cadastro e Descadastro de Usuários:
+* Comando: a(Cadastro) b(Descadastro)  
+* Parâmetro 1: ID
+* Parâmetro 2: Pasta que será Modificada
+*
+*/
