3ed95522c3baf15fa219096383baa099bf4e155c
Vitor Avelino <vitoravelinosena1@gmail.com>
2020-05-20 17:12:19 -0300

    primeiro passo receber e enviar informações

--- routes/esp.js
+++ routes/esp.js
@@ -2,22 +2,7 @@ const express = require("express")
 
 const routeresp = express.Router()
 
-
-
-routeresp.get("/test", (req, res) => {
-    //usudb.connection.query("select * from usuario", function (err, posts, field) {
-       // if (err) throw err;
-       // res.json({ nome: 'tobi', valor:'123'})//, valor:5,time:2})
-       res.send("a\n908a75a3\n2")
-
-
-        //render('admin/usuario', { posts: posts })
-   // })    
-})
-
-
-module.exports = routeresp
-/*
+/* Rota para mandar comandos para o ESP
 *res.send("a\n908a75a3\n2")
 * Comando \n Parâmetro 1 \n Parâmetro 2
 * Comando:
@@ -27,3 +12,36 @@ module.exports = routeresp
 * Parâmetro 2: Pasta que será Modificada
 *
 */
+routeresp.get("/test", (req, res) => {
+    //usudb.connection.query("select * from usuario", function (err, posts, field) {
+       // if (err) throw err;
+       // res.json({ nome: 'tobi', valor:'123'})//, valor:5,time:2})
+       res.send("v\n908a75a3\nAEEE")
+
+
+        //render('admin/usuario', { posts: posts })
+   // })    
+})
+/*Rota para receber 
+*a => Tipo
+*b => 1 Parâmetro
+*c => 2 Parâmetro
+*d => 3 Parâmetro
+*e => 4 Parâmetro
+*/
+/*
+* Para recebimento de log
+* Tipo /N ESP  /TAG     /Resultado/Horário
+* LOG /1 a 100/aaaaaaaa/(a ou n)  / Hora
+*/
+
+routeresp.post("/esptoserver",(req,res)=>{   
+   console.log(req.body.a)
+   console.log(req.body.b)
+   console.log(req.body.c)
+   console.log(req.body.d)
+   console.log(req.body.e)   
+})
+
+
+module.exports = routeresp
